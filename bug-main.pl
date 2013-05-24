90af68587886f66e6ef56bf9f0a73f9ab36f45a5
ptoulis <panos.toulis@gmail.com>
2013-05-24 02:24:01 -0400

    Moved bpgn to .settings

--- bug-main.pl
+++ bug-main.pl
@@ -4,8 +4,6 @@ use warnings;
 require("bug-db.pl");
 require("process-bpgn.pl");
 
-save_pgn_games("/home/ptoulis/A/data/bughouse/export2010.bpgn", 20000 * 1000);
-
 print db_status(), "\n";
 my $gameid = db_all_games()->[0];
 
