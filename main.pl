53478a08ef6a3f5fb3b294009cdfb5abb9a9c019
Panos Toulis <panos.toulis@gmail.com>
2014-02-13 22:55:22 -0500

    New main.pl file

--- main.pl
+++ main.pl
@@ -1,8 +1,5 @@
-use strict;
-use warnings;
+require("terminology.pl");
 
-require("bug-db.pl");
-require("bug-search.pl");
-
-$|=1;
-search_opening("e4 e5 Bc4");
+my $x = generate_DBug("/home/winotgr/A/data/bughouse/export2011.bpgn", 10000);
+my $y = search_db($x, 'w:p@h6    e: 1800 2100');
+print "\nGames found= \n", $y->[0], " ", $y->[1], " ", $y->[2],"\n";
