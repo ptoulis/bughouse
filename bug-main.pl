3adcf9098fd87ef9ca9a4db92b4301a079e7ad6b
ptoulis <panos.toulis@gmail.com>
2014-04-05 23:34:48 -0400

    Changes in the ignore file.

--- bug-main.pl
+++ bug-main.pl
@@ -1,5 +1,5 @@
 require("terminology.pl");
 
-my $x = generate_DBug("/home/winotgr/A/data/bughouse/export2011.bpgn", 10000);
+my $x = generate_DBug("/home/ptoulis/A/data/bughouse/export2010.bpgn", 10000);
 my $y = search_db($x, 'w:p@h6    e: 1800 2100');
 print "\nGames found= \n", $y->[0], " ", $y->[1], " ", $y->[2],"\n";
